a3ba6518ea78ed4a6984f3a3fa9619b11934c07a
Roman Larionov <pcsaveddotcom@gmail.com>
2023-12-20 09:53:10 -0500

    drinks.js created and exported to server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@
 const express = require("express");
 const port = 3000;
 
+//importing data from drinks.js
+const drinks = require("./models/drinks");
+
 //MAIN FUNCTIONALITY
 const app = express();
 
