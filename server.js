e3ddd10a40f3906f9ed9cf33d03fed8e223296f5
Roman Larionov <pcsaveddotcom@gmail.com>
2023-12-20 10:32:45 -0500

    index.ejs created and tested

--- server.js
+++ server.js
@@ -7,13 +7,27 @@ const port = 3000;
 //importing data from drinks.js
 const drinks = require("./models/drinks");
 
+
 //MAIN FUNCTIONALITY
 const app = express();
 
+//using EJS as a viewing engine
+app.set('view engine', 'ejs');  //has to be after the app express
+
 //ROUTES
+//main route
 app.get("/", (req, res) => {
     res.send('Welcome to the Gitpub App!');
 })
+
+//drinks route
+app.get("/drinks", (req, res) => {
+    //res.send(drinks); //initial rendering
+    res.render('index', {drinks});    //rendering new index.ejs file
+})
+
+
+
 
 
 //LISTENER
